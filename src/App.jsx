7c11bf16b1ea48f3e3bb3cecacde772fcfe50e5d
Nayem khan <nayem178548@gmail.com>
2026-01-06 11:42:00 +0000

    accordion and accordionItem added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import Accordion from "./components/accordion/Accordion";
+import AccordionItem from "./components/accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+   <main>
+
+<section>
+
+<h2>Why work with us?</h2>
+<Accordion className='accordion'>
+<AccordionItem className="accordion-item" title="we got 20 years of experience">
+<article>
+  <p> you cant go wrong with us.</p>
+  <p> we are in the business for more than 20 years.</p>
+</article>
+
+</AccordionItem>
+<AccordionItem className="accordion-item" title="we are working with local guides.">
+<article>
+  <p> we are not doing this from office.</p>
+  <p> we are working with the local guides.</p>
+</article>
+
+</AccordionItem>
+
+</Accordion>
+</section>
+
+  </main>
+  );
+
 }
 
 export default App;
